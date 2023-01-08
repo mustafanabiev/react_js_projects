3acb63c9f51e0ab0f8e59bdb56833e13be19a47a
AckmatoffITC <106301056+AckmatoffITC@users.noreply.github.com>
2023-01-08 19:34:15 +0600

    added project

---
+++ onlinebazarkg/js/script.js
@@ -0,0 +1,10 @@
+const heart = document.getElementsByClassName("heart")[0];
+const love = document.getElementById("love");
+
+love.onclick = () => {
+    love.style.backgroundColor = "red";
+}
+
+heart.onclick = () => {
+    heart.style.backgroundColor = "red"
+}
